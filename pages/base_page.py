bb02ff4a01dd937fc7c0943c68c11e9098b8efd2
MariKur <m.kurilovich13@gmail.com>
2026-03-29 15:48:41 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,12 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class BasePage:
-    def __init__(self, driver, base_url='https://www.saucedemo.com/'):
+    def __init__(self, driver, base_url: str = "https://www.saucedemo.com/") -> None:
         self.driver = driver
         self.base_url = base_url
 
-    def visit(self):
-        return self.driver.get(self.base_url)
+    def visit(self) -> None:
+        self.driver.get(self.base_url)
 
     def find_element(self, locator: str):
         return self.driver.find_element(By.CSS_SELECTOR, locator)
