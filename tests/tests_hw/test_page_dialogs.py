2a7b0b8f115cc73307d848195cefd4574981c277
MariKur <m.kurilovich13@gmail.com>
2026-04-04 15:15:21 +0300

    Update test_page_dialogs.py

--- tests/tests_hw/test_page_dialogs.py
+++ tests/tests_hw/test_page_dialogs.py
@@ -1,16 +1,13 @@
 from pages.modal_dialogs import ModalDialogs
 
 
-# 1. Проверка количества кнопок
 def test_modal_elements(browser):
     page = ModalDialogs(browser)
     page.visit()
 
-    # проверка, что кнопок подменю 5
     assert page.submenu_buttons.count_elements() == 5
 
 
-# 2. Проверка навигации
 def test_navigation_modal(browser):
     page = ModalDialogs(browser)
     page.visit()
@@ -24,17 +21,17 @@ def test_navigation_modal(browser):
     # шаг назад
     page.back()
 
-    # установить размер окна
+    # установить размеры окна
     page.set_window_size(900, 400)
 
     # шаг вперед
     page.forward()
 
-    # проверка URL главной страницы
-    assert page.get_current_url() == "https://demoqa.com/"
+    # проверка url главной страницы
+    assert page.get_current_url() == 'https://demoqa.com/'
 
-    # проверка title
-    assert page.get_title() == "DEMOQA"
+    # проверка title главной страницы
+    assert page.get_title() == 'DEMOQA'
 
-    # вернуть размер окна
+    # вернуть размеры по умолчанию
     page.set_window_size(1000, 1000)
