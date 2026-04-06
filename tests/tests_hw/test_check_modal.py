a8c9ef3c376c7bad4f8f86f29eb7c403aa60de4a
MariKur <m.kurilovich13@gmail.com>
2026-04-06 22:48:38 +0300

    Update test_check_modal.py

--- tests/tests_hw/test_check_modal.py
+++ tests/tests_hw/test_check_modal.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 
 from pages.base_page import BasePage
@@ -16,7 +17,7 @@ def is_page_available(url: str) -> bool:
 
 @pytest.mark.skipif(
     not is_page_available("https://demoqa.com/modal-dialogs"),
-    reason="Страница modal-dialogs недоступна",
+    reason="Страница https://demoqa.com/modal-dialogs недоступна"
 )
 def test_check_modal(browser):
     page = BasePage(browser, "https://demoqa.com/modal-dialogs")
@@ -24,21 +25,30 @@ def test_check_modal(browser):
 
     small_modal_button = BaseComponent(browser, "#showSmallModal")
     large_modal_button = BaseComponent(browser, "#showLargeModal")
-    modal_window = BaseComponent(browser, ".modal-content")
+
+    small_modal = BaseComponent(browser, ".modal-content")
     close_small_button = BaseComponent(browser, "#closeSmallModal")
+
     close_large_button = BaseComponent(browser, "#closeLargeModal")
 
-    assert small_modal_button.get_text() == "Small modal"
-    assert large_modal_button.get_text() == "Large modal"
-
     # Small modal
+    assert small_modal_button.get_text() == "Small modal"
     small_modal_button.click()
-    assert modal_window.is_visible()
+    assert small_modal.is_visible()
     close_small_button.click()
-    WebDriverWait(browser, 5).until(lambda d: not modal_window.is_visible())
+
+    try:
+        WebDriverWait(browser, 5).until(lambda d: not small_modal.is_visible())
+    except TimeoutException:
+        assert False, "Small modal не закрылось"
 
     # Large modal
+    assert large_modal_button.get_text() == "Large modal"
     large_modal_button.click()
-    assert modal_window.is_visible()
+    assert small_modal.is_visible()
     close_large_button.click()
-    WebDriverWait(browser, 5).until(lambda d: not modal_window.is_visible())
+
+    try:
+        WebDriverWait(browser, 5).until(lambda d: not small_modal.is_visible())
+    except TimeoutException:
+        assert False, "Large modal не закрылось"
